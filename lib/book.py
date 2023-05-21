860bd34346b07dfb8fb95ee926141c12d037bc83
johnthairukamau <tkamau82@gmail.com>
2023-05-22 01:06:02 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def check_page_count(page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+
+    page_count = "20"
+    check_page_count(page_count)  # Prints "page_count must be an integer" since "20" is not an integer
+
+    def turn_page():
+        print("Flipping the page...wow, you read fast!")
+        
+    turn_page()
+
+# Creating a new instance of the Book class
+my_book = Book("Python Programming", 400)
+
+# Accessing the attributes
+print(my_book.title)       # Output: Python Programming
+print(my_book.page_count)  # Output: 400
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def set_values(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def get_brand(self):
+        return self.brand
+
+    def get_size(self):
+        return self.size
+    
+    def print_size_error(size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+
+    print_size_error(10)
+    print_size_error("not an integer")
+
+    
+    def __init__(self, name):
+        self.name = name
+        self.condition = 'Used'  # Initial condition is 'Used'
+
+    def repair(self):
+        self.condition = 'New'  # Set condition to 'New' after repair
+
+
+        # Example usage:
+        item = Shoe('Laptop')
+        print(item.condition)  # Output: Used
+
+        # item.repair()
+        # print(item.condition)  # Output: New
+
+
+product = Shoe("Nike", "L")
+print(product.get_brand())  # Output: Nike
+print(product.get_size())   # Output: L
+
+product.set_values("Adidas", "M")
+print(product.get_brand())  # Output: Adidas
+print(product.get_size())   # Output: M
